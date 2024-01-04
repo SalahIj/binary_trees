715ce93b6f9147df813c18f850c9423e2e162b67
zakaria1011 <grarizakaria@gmail.com>
2024-01-04 17:34:20 +0100

    zakaria

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - the function name.
+ * @tree: the input of the function.
+ * Return: the result
+*/
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (0);
+
+if (tree->left == NULL && tree->right == NULL)
+return (1);
+if (tree->left != NULL || tree->right != NULL)
+return (binary_tree_is_full(tree->left) &&
+binary_tree_is_full(tree->right));
+return (0);
+}
