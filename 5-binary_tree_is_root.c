ab9f87079f3002e9b79e49410e06606aa47c2ed5
Ijbar Salah Eddine <ijbarbusiness@gmail.com>
2024-01-04 15:18:33 +0000

    Salah

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - the function name
+ * @node: the input of the function
+ * Return: the result
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->parent == NULL)
+		return (1);
+	return (0);
+}
